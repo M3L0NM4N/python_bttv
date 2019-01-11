1f43a95ecc7366809bdeb422da29e33ffc58671c
Milaan Dahiya <milaandahiya@gmail.com>
2019-01-11 13:41:56 -0600

    Uploading baseline files
    (not working yet)

---
+++ emote_grabber.py
@@ -0,0 +1,19 @@
+import urllib, urllib.request
+import os
+import json
+
+if not os.path.exists('./emotes'):
+    os.makedirs('./emotes')
+print('Saving emotes to folder: ' + os.path.abspath('./emotes') + '...')
+print('Grabbing emote list...')
+emotes = json.load(urllib.request.urlopen('https://twitchemotes.com/api_cache/v2/global.json'))
+for code, emote in emotes['emotes'].items():
+    print('Downloading: ' + code + '...')
+    urllib.urlretrieve('http:' + emotes['template']['large'].replace('{image_id}', str(emote['image_id'])),
+                       './emotes/' + code + '.png')
+print('Done! Kappa')
+
+
+
+
+#u/rtainc
