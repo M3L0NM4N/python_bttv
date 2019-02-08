ada36f3e568f88bb09293e801ceb3d89a6a604db
Milaan Dahiya <milaandahiya@gmail.com>
2019-02-08 13:31:51 -0600

    working listgrabber.py
    parses through streamerslist.txt with regex, prints output to sortedstreamers.txt

--- listgrabber.py
+++ listgrabber.py
@@ -1,5 +1,12 @@
 import re
 
 #using https://www.twitchmetrics.net/channels/viewership
-streamers = re.findall("<h5 class=\"mr-2 mb-0\">(.*)</h5", open("streamerslist.txt"))
-print(streamers)
+
+#gets list of top 250 viewed streamers on Twitch
+streamerslist = open("streamerslist.txt","r", encoding="utf8")
+streamerslist = streamerslist.read()
+streamerslist = re.findall("<h5 class=\"mr-2 mb-0\">(.*)</h5", streamerslist)
+
+sortedstreamers = open("sortedstreamers.txt", "w", encoding="utf8")
+for x in streamerslist:
+    sortedstreamers.write(x + "\n")
